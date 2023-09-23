e8f36f6068c67b0e21d35f01a6c96ac672b85f8a
Norbert <107361913+n0rb33rt@users.noreply.github.com>
2023-09-23 11:46:09 +0300

    Time: 1 ms (90.45%), Space: 44.2 MB (32.39%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,41 +13,25 @@
  *     }
  * }
  */
-class TreeNodeLevelPair {
-    private TreeNode node;
-    private int level;
-
-    public TreeNodeLevelPair(TreeNode node, int level) {
-        this.node = node;
-        this.level = level;
-    }
-
-    public TreeNode getNode() {
-        return node;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-}
-
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null)
             return result;
-        Queue<TreeNodeLevelPair> queue = new LinkedList<>();
-        queue.add(new TreeNodeLevelPair(root,0));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
         while (!queue.isEmpty()) {
-            TreeNodeLevelPair presentNode = queue.remove();
-            Integer currentNodeLevel = presentNode.getLevel();
-            if(result.size() < currentNodeLevel+1)
-                result.add(currentNodeLevel, new ArrayList<>());
-            result.get(currentNodeLevel).add(presentNode.getNode().val);
-            if (presentNode.getNode().left != null)
-                queue.add(new TreeNodeLevelPair(presentNode.getNode().left,currentNodeLevel+1));
-            if (presentNode.getNode().right != null)
-                 queue.add(new TreeNodeLevelPair(presentNode.getNode().right,currentNodeLevel+1));
+            int levels = queue.size();
+            List<Integer> subLevels = new ArrayList<>();
+            for(int i=0;i<levels;i++){
+                TreeNode currentNode = queue.remove();
+                if(currentNode.left!=null)
+                    queue.add(currentNode.left);
+                if(currentNode.right!=null)
+                    queue.add(currentNode.right);
+                subLevels.add(currentNode.val);
+            }
+            result.add(subLevels);
         }
         return result;
     }
